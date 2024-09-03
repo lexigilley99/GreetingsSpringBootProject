64f736ae5f56a13d6479beb696dda93e01721059
lexigilley99 <lexigilley99@gmail.com>
2024-09-03 12:35:37 -0400

    added message and main app

---
+++ src/main/java/org/example/greetings/GreetingController.java
@@ -0,0 +1,14 @@
+package org.example.greetings;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String getGreeting() {
+        return "Hello, World!";
+    }
+}
+
