faa28ece910b70b44e9c4751ac77825cceb8a044
lexigilley99 <lexigilley99@gmail.com>
2024-09-03 10:10:45 -0400

    Initial commit

---
+++ src/test/java/org/example/greetings/GreetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.greetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
